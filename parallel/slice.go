302db336451560c8632c47e14d5268d79536f6c4
Azer0s <ariel@simulevski.at>
2022-03-13 23:20:04 +0100

    Simplify PartitionBy implementation

--- parallel/slice.go
+++ parallel/slice.go
@@ -62,33 +62,11 @@ func GroupBy[T any, U comparable](collection []T, iteratee func(T) U) map[U][]T 
 // `iteratee` is call in parallel.
 func PartitionBy[T any, K comparable](collection []T, iteratee func(x T) K) [][]T {
 	result := [][]T{}
-	seen := map[K]int{}
+	groups := GroupBy(collection, iteratee)
 
-	var mu sync.Mutex
-	var wg sync.WaitGroup
-	wg.Add(len(collection))
-
-	for _, item := range collection {
-		go func(_item T) {
-			key := iteratee(_item)
-
-			mu.Lock()
-
-			resultIndex, ok := seen[key]
-			if !ok {
-				resultIndex = len(result)
-				seen[key] = resultIndex
-				result = append(result, []T{})
-			}
-
-			result[resultIndex] = append(result[resultIndex], _item)
-
-			mu.Unlock()
-			wg.Done()
-		}(item)
+	for _, v := range groups {
+		result = append(result, v)
 	}
-
-	wg.Wait()
 
 	return result
 }
