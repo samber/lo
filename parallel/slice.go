91a68ed7356c9550c42fbc39adf3a8909781a382
Bin-Huang <tohuangbin@gmail.com>
2022-04-02 15:10:18 +0800

    doc: methods

--- parallel/slice.go
+++ parallel/slice.go
@@ -4,6 +4,9 @@ import "sync"
 
 // Map manipulates a slice and transforms it to a slice of another type.
 // `iteratee` is call in parallel. Result keep the same order.
+// You also can control the conurrency limit by optional ParallelOption to limit the maximum number of
+// concurrent `iteratee` goroutines running at the same time, just like
+// `parallel.Map(list, iteratee, parallel.Option().Concurrency(10))`.
 func Map[T any, R any](collection []T, iteratee func(T, int) R, options ...*ParallelOption) []R {
 	result := make([]R, len(collection))
 
@@ -18,6 +21,9 @@ func Map[T any, R any](collection []T, iteratee func(T, int) R, options ...*Para
 
 // ForEach iterates over elements of collection and invokes iteratee for each element.
 // `iteratee` is call in parallel.
+// You also can control the conurrency limit by optional ParallelOption to limit the maximum number of
+// concurrent `iteratee` goroutines running at the same time, just like
+// `parallel.ForEach(list, iteratee, parallel.Option().Concurrency(10))`.
 func ForEach[T any](collection []T, iteratee func(T, int), options ...*ParallelOption) {
 	var wg sync.WaitGroup
 	var concurrencyChn chan bool
@@ -45,6 +51,9 @@ func ForEach[T any](collection []T, iteratee func(T, int), options ...*ParallelO
 // Times invokes the iteratee n times, returning an array of the results of each invocation.
 // The iteratee is invoked with index as argument.
 // `iteratee` is call in parallel.
+// You also can control the conurrency limit by optional ParallelOption to limit the maximum number of
+// concurrent `iteratee` goroutines running at the same time, just like
+// `parallel.Times(count, iteratee, parallel.Option().Concurrency(10))`.
 func Times[T any](count int, iteratee func(int) T, options ...*ParallelOption) []T {
 	var concurrencyChn chan bool
 
@@ -76,6 +85,9 @@ func Times[T any](count int, iteratee func(int) T, options ...*ParallelOption) [
 
 // GroupBy returns an object composed of keys generated from the results of running each element of collection through iteratee.
 // `iteratee` is call in parallel.
+// You also can control the conurrency limit by optional ParallelOption to limit the maximum number of
+// concurrent `iteratee` goroutines running at the same time, just like
+// `parallel.GroupBy(list, iteratee, parallel.Option().Concurrency(10))`.
 func GroupBy[T any, U comparable](collection []T, iteratee func(T) U, options ...*ParallelOption) map[U][]T {
 	result := map[U][]T{}
 	var mu sync.Mutex
@@ -102,6 +114,9 @@ func GroupBy[T any, U comparable](collection []T, iteratee func(T) U, options ..
 // determined by the order they occur in collection. The grouping is generated from the results
 // of running each element of collection through iteratee.
 // `iteratee` is call in parallel.
+// You also can control the conurrency limit by optional ParallelOption to limit the maximum number of
+// concurrent `iteratee` goroutines running at the same time, just like
+// `parallel.PartitionBy(list, iteratee, parallel.Option().Concurrency(10))`.
 func PartitionBy[T any, K comparable](collection []T, iteratee func(x T) K, options ...*ParallelOption) [][]T {
 	result := [][]T{}
 
