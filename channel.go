ba7adf605f25e712d0204f4dccca0e5ec46951b6
Samuel Berthe <dev@samuel-berthe.fr>
2022-04-14 21:59:07 +0200

    feat(channels): add ToChannel + Generator + Batch + BatchWithTimeout (WIP)

--- channel.go
+++ channel.go
@@ -149,3 +149,75 @@ func DispatchingStrategyMost[T any](msg T, index uint64, channels []<-chan T) in
 		return len(channels[item]) > len(channels[max]) && channelIsNotFull(channels[item])
 	})
 }
+
+// ToChannel returns a read-only channels of collection elements.
+func ToChannel[T any](bufferSize int, collection []T) <-chan T {
+	ch := make(chan T, bufferSize)
+
+	go func() {
+		for _, item := range collection {
+			ch <- item
+		}
+
+		close(ch)
+	}()
+
+	return ch
+}
+
+// Generator implements the generator design pattern.
+func Generator[T any](bufferSize int, generator func(yield func(T))) <-chan T {
+	ch := make(chan T, bufferSize)
+
+	go func() {
+		// WARNING: infinite loop
+		generator(func(t T) {
+			ch <- t
+		})
+
+		close(ch)
+	}()
+
+	return ch
+}
+
+// Batch creates a slice of n elements from a channel. Returns the slice and the slice length.
+func Batch[T any](ch <-chan T, size int) (collection []T, length int) {
+	buffer := make([]T, 0, size)
+	index := 0
+
+	for ; index < size; index++ {
+		item, ok := <-ch
+		if !ok {
+			return buffer, index
+		}
+
+		buffer = append(buffer, item)
+	}
+
+	return buffer, index
+}
+
+// BatchWithTimeout creates a slice of n elements from a channel, with timeout. Returns the slice and the slice length.
+func BatchWithTimeout[T any](ch <-chan T, size int, timeout time.Duration) (collection []T, length int) {
+	expire := time.After(timeout)
+
+	buffer := make([]T, 0, size)
+	index := 0
+
+	for ; index < size; index++ {
+		select {
+		case item, ok := <-ch:
+			if !ok {
+				return buffer, index
+			}
+
+			buffer = append(buffer, item)
+
+		case <-expire:
+			return buffer, index
+		}
+	}
+
+	return buffer, index
+}
