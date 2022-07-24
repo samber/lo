f2175ff5c2d5215dc80568e977e21b91223a1dcd
sleagon <shanyy163@163.com>
2022-07-25 04:04:55 +0800

    feat: add MapToSlice method (#157)
    Change-Id: I97d3e58ddc36e65231e5e76dc9239aa52a4a7613

--- map.go
+++ map.go
@@ -172,3 +172,14 @@ func MapValues[K comparable, V any, R any](in map[K]V, iteratee func(V, K) R) ma
 
 	return result
 }
+
+// MapToSlice transforms a map into a slice based on specific iteratee
+func MapToSlice[K comparable, V any, R any](in map[K]V, iteratee func(V, K) R) []R {
+	result := make([]R, 0, len(in))
+
+	for k, v := range in {
+		result = append(result, iteratee(v, k))
+	}
+
+	return result
+}
