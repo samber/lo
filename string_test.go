9ec076e4f62bf41b6c4a5491e6428a27d70a3c23
Samuel Berthe <dev@samuel-berthe.fr>
2023-03-20 17:59:52 +0100

    test: adding some tests to Substring (see #288)

--- string_test.go
+++ string_test.go
@@ -75,6 +75,7 @@ func TestSubstring(t *testing.T) {
 	str11 := Substring("hello", -4, 1)
 	str12 := Substring("hello", -4, math.MaxUint)
 	str13 := Substring("🏠🐶🐱", 0, 2)
+	str14 := Substring("你好，世界", 0, 3)
 
 	is.Equal("", str1)
 	is.Equal("", str2)
@@ -89,6 +90,7 @@ func TestSubstring(t *testing.T) {
 	is.Equal("e", str11)
 	is.Equal("ello", str12)
 	is.Equal("🏠🐶", str13)
+	is.Equal("你好，", str14)
 }
 
 func TestRuneLength(t *testing.T) {
