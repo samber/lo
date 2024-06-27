97074eeae7bb8a40bf8dcb2af4b0a53f686d514a
Usman Ahmed <42866718+usman1100@users.noreply.github.com>
2024-06-27 11:45:28 +0000

    Adding Mean and MeanBy (#414)
    * feat: create Mean for ints and floats
    
    * feat: create MeanBy for ints and floats
    
    * chore: add example tests for mean Mean and MeanBy

--- math_test.go
+++ math_test.go
@@ -97,3 +97,33 @@ func TestSumBy(t *testing.T) {
 	is.Equal(result4, uint32(0))
 	is.Equal(result5, complex128(6_6))
 }
+
+func TestMean(t *testing.T) {
+	t.Parallel()
+	is := assert.New(t)
+
+	result1 := Mean([]float32{2.3, 3.3, 4, 5.3})
+	result2 := Mean([]int32{2, 3, 4, 5})
+	result3 := Mean([]uint32{2, 3, 4, 5})
+	result4 := Mean([]uint32{})
+
+	is.Equal(result1, float32(3.7250001))
+	is.Equal(result2, int32(3))
+	is.Equal(result3, uint32(3))
+	is.Equal(result4, uint32(0))
+}
+
+func TestMeanBy(t *testing.T) {
+	t.Parallel()
+	is := assert.New(t)
+
+	result1 := MeanBy([]float32{2.3, 3.3, 4, 5.3}, func(n float32) float32 { return n })
+	result2 := MeanBy([]int32{2, 3, 4, 5}, func(n int32) int32 { return n })
+	result3 := MeanBy([]uint32{2, 3, 4, 5}, func(n uint32) uint32 { return n })
+	result4 := MeanBy([]uint32{}, func(n uint32) uint32 { return n })
+
+	is.Equal(result1, float32(3.7250001))
+	is.Equal(result2, int32(3))
+	is.Equal(result3, uint32(3))
+	is.Equal(result4, uint32(0))
+}
