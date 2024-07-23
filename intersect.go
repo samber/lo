1f0fa6e7cf43a988f5f0a6e41148b3aa4488d2ae
liujun <995672751@qq.com>
2024-07-23 10:10:58 +0800

    Intersection deweighting

--- intersect.go
+++ intersect.go
@@ -102,6 +102,7 @@ func Intersect[T comparable, Slice ~[]T](list1 Slice, list2 Slice) Slice {
 	for i := range list2 {
 		if _, ok := seen[list2[i]]; ok {
 			result = append(result, list2[i])
+			delete(seen, list2[i])
 		}
 	}
 
