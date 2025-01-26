382dc1373a8d417923b04af09cc8211640984686
Samuel Berthe <dev@samuel-berthe.fr>
2025-01-26 15:55:45 +0100

    Update find.go

--- find.go
+++ find.go
@@ -473,7 +473,7 @@ func Nth[T any, N constraints.Integer](collection []T, nth N) (T, error) {
 	return collection[l+n], nil
 }
 
-// randomIntGenerator is a function that should return a random integer in the range [0, n-1]
+// randomIntGenerator is a function that should return a random integer in the range [0, n)
 // where n is the parameter passed to the randomIntGenerator.
 type randomIntGenerator func(int) int
 
