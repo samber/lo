5e24c1cdd99f77842db4210e7adfeb00ff2d3bc6
sr9000 <no@mail.org>
2025-07-19 21:10:20 +0300

    loset

---
+++ loset/set.go
@@ -0,0 +1,35 @@
+package loset
+
+import "iter"
+
+type Set[T comparable] = map[T]struct{}
+
+func FromSlice[T comparable, Slice ~[]T](xs Slice) Set[T] {
+	set := make(Set[T], len(xs))
+
+	for _, x := range xs {
+		set[x] = struct{}{}
+	}
+
+	return set
+}
+
+func Collect[T comparable, Seq iter.Seq[T]](xs Seq) Set[T] {
+	set := make(Set[T])
+
+	for x := range xs {
+		set[x] = struct{}{}
+	}
+
+	return set
+}
+
+func ToSlice[T comparable, S ~Set[T]](set S) []T {
+	slice := make([]T, 0, len(set))
+
+	for x := range set {
+		slice = append(slice, x)
+	}
+
+	return slice
+}
