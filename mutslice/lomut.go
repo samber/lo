9c0d9c26020d3cef4d2ef87344663fd72dc1605d
sr9000 <no@mail.org>
2025-07-20 13:36:21 +0300

    take address of slice values

--- mutslice/lomut.go
+++ mutslice/lomut.go
@@ -1,3 +1,13 @@
 // Package mutslice provides operations that mutates slices in place.
 // Or the result shares memory with the input slice.
 package mutslice
+
+// ToPointers converts a slice of values to a slice of pointers to those values.
+func ToPointers[T any, Slice ~[]T](xs Slice) []*T {
+	result := make([]*T, len(xs))
+	for i := range xs {
+		result[i] = &xs[i]
+	}
+
+	return result
+}
