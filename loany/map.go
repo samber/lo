08783e0a76367f504f63f40650d5e8e4e9038b76
sr9000 <no@mail.org>
2025-07-22 09:15:39 +0300

    loany map

--- loany/map.go
+++ loany/map.go
@@ -1,6 +1,6 @@
 package loany
 
-func Map[K comparable, V any, Map ~map[K]V](m Map) map[K]any {
+func Map[Map ~map[K]V, K comparable, V any](m Map) map[K]any {
 	result := make(map[K]any, len(m))
 	for k, v := range m {
 		result[k] = v
@@ -11,7 +11,7 @@ func Map[K comparable, V any, Map ~map[K]V](m Map) map[K]any {
 
 // TypedMap converts a map of any type to a map of a specific type V.
 // Values that cannot be converted to V will be omitted.
-func TypedMap[V any, K comparable, Map ~map[K]any](m Map) map[K]V {
+func TypedMap[Map ~map[K]any, V any, K comparable](m Map) map[K]V {
 	result := make(map[K]V, len(m))
 	for k, v := range m {
 		if val, ok := v.(V); ok {
