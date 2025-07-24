2e3362751f884f20e5cf4f469b66f12332521c8f
sr9000 <no@mail.org>
2025-07-24 10:22:04 +0300

    lomap min max

--- lomap/min_n_max.go
+++ lomap/min_n_max.go
@@ -5,6 +5,22 @@ import (
 	"github.com/samber/lo/lotup"
 )
 
+func MinKey[Map ~map[K]V, K cmp.Ordered, V any](m Map) K {
+	if len(m) == 0 {
+		var zero K
+		return zero
+	}
+
+	res := lotup.First(Any(m))
+	for k := range m {
+		if k < res {
+			res = k
+		}
+	}
+
+	return res
+}
+
 func MinVal[Map ~map[K]V, K comparable, V cmp.Ordered](m Map) V {
 	if len(m) == 0 {
 		var zero V
@@ -19,4 +35,220 @@ func MinVal[Map ~map[K]V, K comparable, V cmp.Ordered](m Map) V {
 	}
 
 	return res
+}
+
+func ArgMinVal[Map ~map[K]V, K comparable, V cmp.Ordered](m Map) K {
+	if len(m) == 0 {
+		var zero K
+		return zero
+	}
+
+	key, res := Any(m)
+	for k, v := range m {
+		if v < res {
+			key, res = k, v
+		}
+	}
+
+	return key
+}
+
+func Min[Map ~map[K]V, K comparable, V any, R cmp.Ordered](m Map, fkey func(V) R) (V, R) {
+	if len(m) == 0 {
+		var zero V
+		var zeroR R
+		return zero, zeroR
+	}
+
+	val := lotup.Second(Any(m))
+	rank := fkey(val)
+	for _, v := range m {
+		r := fkey(v)
+		if r < rank {
+			val, rank = v, r
+		}
+	}
+
+	return val, rank
+}
+
+func ArgMin[Map ~map[K]V, K comparable, V any, R cmp.Ordered](m Map, fkey func(V) R) (K, R) {
+	if len(m) == 0 {
+		var zero K
+		var zeroR R
+		return zero, zeroR
+	}
+
+	key, v := Any(m)
+	rank := fkey(v)
+	for k, v := range m {
+		r := fkey(v)
+		if r < rank {
+			key, rank = k, r
+		}
+	}
+
+	return key, rank
+}
+
+func KMin[Map ~map[K]V, K comparable, V any, R cmp.Ordered](m Map, fkey func(K, V) R) (V, R) {
+	if len(m) == 0 {
+		var zero V
+		var zeroR R
+		return zero, zeroR
+	}
+
+	k, val := Any(m)
+	rank := fkey(k, val)
+	for k, v := range m {
+		r := fkey(k, v)
+		if r < rank {
+			val, rank = v, r
+		}
+	}
+
+	return val, rank
+}
+
+func KArgMin[Map ~map[K]V, K comparable, V any, R cmp.Ordered](m Map, fkey func(K, V) R) (K, R) {
+	if len(m) == 0 {
+		var zero K
+		var zeroR R
+		return zero, zeroR
+	}
+
+	key, v := Any(m)
+	rank := fkey(key, v)
+	for k, v := range m {
+		r := fkey(k, v)
+		if r < rank {
+			key, rank = k, r
+		}
+	}
+
+	return key, rank
+}
+
+func MaxKey[Map ~map[K]V, K cmp.Ordered, V any](m Map) K {
+	if len(m) == 0 {
+		var zero K
+		return zero
+	}
+
+	res := lotup.First(Any(m))
+	for k := range m {
+		if k > res {
+			res = k
+		}
+	}
+
+	return res
+}
+
+func MaxVal[Map ~map[K]V, K comparable, V cmp.Ordered](m Map) V {
+	if len(m) == 0 {
+		var zero V
+		return zero
+	}
+
+	res := lotup.Second(Any(m))
+	for _, v := range m {
+		if v > res {
+			res = v
+		}
+	}
+
+	return res
+}
+
+func ArgMaxVal[Map ~map[K]V, K comparable, V cmp.Ordered](m Map) K {
+	if len(m) == 0 {
+		var zero K
+		return zero
+	}
+
+	key, res := Any(m)
+	for k, v := range m {
+		if v > res {
+			key, res = k, v
+		}
+	}
+
+	return key
+}
+
+func Max[Map ~map[K]V, K comparable, V any, R cmp.Ordered](m Map, fkey func(V) R) (V, R) {
+	if len(m) == 0 {
+		var zero V
+		var zeroR R
+		return zero, zeroR
+	}
+
+	val := lotup.Second(Any(m))
+	rank := fkey(val)
+	for _, v := range m {
+		r := fkey(v)
+		if r > rank {
+			val, rank = v, r
+		}
+	}
+
+	return val, rank
+}
+
+func ArgMax[Map ~map[K]V, K comparable, V any, R cmp.Ordered](m Map, fkey func(V) R) (K, R) {
+	if len(m) == 0 {
+		var zero K
+		var zeroR R
+		return zero, zeroR
+	}
+
+	key, v := Any(m)
+	rank := fkey(v)
+	for k, v := range m {
+		r := fkey(v)
+		if r > rank {
+			key, rank = k, r
+		}
+	}
+
+	return key, rank
+}
+
+func KMax[Map ~map[K]V, K comparable, V any, R cmp.Ordered](m Map, fkey func(K, V) R) (V, R) {
+	if len(m) == 0 {
+		var zero V
+		var zeroR R
+		return zero, zeroR
+	}
+
+	k, val := Any(m)
+	rank := fkey(k, val)
+	for k, v := range m {
+		r := fkey(k, v)
+		if r > rank {
+			val, rank = v, r
+		}
+	}
+
+	return val, rank
+}
+
+func KArgMax[Map ~map[K]V, K comparable, V any, R cmp.Ordered](m Map, fkey func(K, V) R) (K, R) {
+	if len(m) == 0 {
+		var zero K
+		var zeroR R
+		return zero, zeroR
+	}
+
+	key, v := Any(m)
+	rank := fkey(key, v)
+	for k, v := range m {
+		r := fkey(k, v)
+		if r > rank {
+			key, rank = k, r
+		}
+	}
+
+	return key, rank
 }
