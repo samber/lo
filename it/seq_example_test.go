04aa85e5ec6b5eb466b613286bc60894a8cff29c
Nathan Baulch <nathan.baulch@gmail.com>
2025-09-26 08:19:54 +1000

    feat: implement Shuffle helper

--- it/seq_example_test.go
+++ it/seq_example_test.go
@@ -254,6 +254,14 @@ func ExampleInterleave() {
 	// [1 2 3 4 5 6 7 8 9 10]
 }
 
+func ExampleShuffle() {
+	list := slices.Values([]int{0, 1, 2, 3, 4, 5})
+
+	result := slices.Collect(Shuffle(list))
+
+	fmt.Printf("%v", result)
+}
+
 func ExampleReverse() {
 	list := slices.Values([]int{0, 1, 2, 3, 4, 5})
 
