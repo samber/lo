51ae28b1f49b2d7df34caff061996b38837ed3fc
Nathan Baulch <nathan.baulch@gmail.com>
2025-09-26 15:12:10 +1000

    feat: implement SeqToChannel2 helper

--- it/channel_test.go
+++ it/channel_test.go
@@ -3,9 +3,11 @@
 package it
 
 import (
+	"maps"
 	"slices"
 	"testing"
 
+	"github.com/samber/lo"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -24,6 +26,24 @@ func TestSeqToChannel(t *testing.T) {
 	is.Equal(2, r2)
 	is.True(ok3)
 	is.Equal(3, r3)
+
+	_, ok4 := <-ch
+	is.False(ok4)
+}
+
+func TestSeqToChannel2(t *testing.T) {
+	t.Parallel()
+	is := assert.New(t)
+
+	ch := SeqToChannel2(2, maps.All(map[string]int{"a": 1, "b": 2, "c": 3}))
+
+	r1, ok1 := <-ch
+	r2, ok2 := <-ch
+	r3, ok3 := <-ch
+	is.True(ok1)
+	is.True(ok2)
+	is.True(ok3)
+	is.ElementsMatch([]lo.Tuple2[string, int]{{A: "a", B: 1}, {A: "b", B: 2}, {A: "c", B: 3}}, []lo.Tuple2[string, int]{r1, r2, r3})
 
 	_, ok4 := <-ch
 	is.False(ok4)
