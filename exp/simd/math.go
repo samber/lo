b945efd840b88f028058d6247b2ed88076b6c67f
Samuel Berthe <dev@samuel-berthe.fr>
2026-02-17 17:44:21 +0100

    fix(simd): call SSE fallback instead of lo.Sum for default helpers

--- exp/simd/math.go
+++ exp/simd/math.go
@@ -14,7 +14,7 @@ func SumInt8[T ~int8](collection []T) T {
 	} else if archsimd.X86.AVX2() {
 		return SumInt8x32(collection)
 	}
-	return lo.Sum(collection)
+	return lo.SumInt8x16(collection)
 }
 
 func SumInt16[T ~int16](collection []T) T {
@@ -23,7 +23,7 @@ func SumInt16[T ~int16](collection []T) T {
 	} else if archsimd.X86.AVX2() {
 		return SumInt16x16(collection)
 	}
-	return lo.Sum(collection)
+	return lo.SumInt16x8(collection)
 }
 
 func SumInt32[T ~int32](collection []T) T {
@@ -32,7 +32,7 @@ func SumInt32[T ~int32](collection []T) T {
 	} else if archsimd.X86.AVX2() {
 		return SumInt32x8(collection)
 	}
-	return lo.Sum(collection)
+	return lo.SumInt32x4(collection)
 }
 
 func SumInt64[T ~int64](collection []T) T {
@@ -41,7 +41,7 @@ func SumInt64[T ~int64](collection []T) T {
 	} else if archsimd.X86.AVX2() {
 		return SumInt64x4(collection)
 	}
-	return lo.Sum(collection)
+	return lo.SumInt64x2(collection)
 }
 
 func SumUint8[T ~uint8](collection []T) T {
@@ -50,7 +50,7 @@ func SumUint8[T ~uint8](collection []T) T {
 	} else if archsimd.X86.AVX2() {
 		return SumUint8x32(collection)
 	}
-	return lo.Sum(collection)
+	return lo.SumUint8x16(collection)
 }
 
 func SumUint16[T ~uint16](collection []T) T {
@@ -59,7 +59,7 @@ func SumUint16[T ~uint16](collection []T) T {
 	} else if archsimd.X86.AVX2() {
 		return SumUint16x16(collection)
 	}
-	return lo.Sum(collection)
+	return lo.SumUint16x8(collection)
 }
 
 func SumUint32[T ~uint32](collection []T) T {
@@ -68,7 +68,7 @@ func SumUint32[T ~uint32](collection []T) T {
 	} else if archsimd.X86.AVX2() {
 		return SumUint32x8(collection)
 	}
-	return lo.Sum(collection)
+	return lo.SumUint32x4(collection)
 }
 
 func SumUint64[T ~uint64](collection []T) T {
@@ -77,7 +77,7 @@ func SumUint64[T ~uint64](collection []T) T {
 	} else if archsimd.X86.AVX2() {
 		return SumUint64x4(collection)
 	}
-	return lo.Sum(collection)
+	return lo.SumUint64x2(collection)
 }
 
 func SumFloat32[T ~float32](collection []T) T {
@@ -86,7 +86,7 @@ func SumFloat32[T ~float32](collection []T) T {
 	} else if archsimd.X86.AVX2() {
 		return SumFloat32x8(collection)
 	}
-	return lo.Sum(collection)
+	return lo.SumFloat32x4(collection)
 }
 
 func SumFloat64[T ~float64](collection []T) T {
@@ -95,5 +95,5 @@ func SumFloat64[T ~float64](collection []T) T {
 	} else if archsimd.X86.AVX2() {
 		return SumFloat64x4(collection)
 	}
-	return lo.Sum(collection)
+	return lo.SumFloat64x2(collection)
 }
