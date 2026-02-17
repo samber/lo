c4d17ca847b7cad46a143ceb7d07e79322fab504
Samuel Berthe <dev@samuel-berthe.fr>
2026-02-17 17:53:46 +0100

    feat(simd): cache simd features on package init to avoid repeated checks

--- exp/simd/math.go
+++ exp/simd/math.go
@@ -3,97 +3,135 @@
 package simd
 
 import (
-	"simd/archsimd"
-
 	"github.com/samber/lo"
 )
 
 func SumInt8[T ~int8](collection []T) T {
-	if archsimd.X86.AVX512() {
+	switch currentSimdFeature {
+	case simdFeatureAVX512:
 		return SumInt8x64(collection)
-	} else if archsimd.X86.AVX2() {
+	case simdFeatureAVX2:
 		return SumInt8x32(collection)
+	case simdFeatureAVX:
+		return SumInt8x16(collection)
+	default:
+		return lo.Sum(collection)
 	}
-	return lo.SumInt8x16(collection)
 }
 
 func SumInt16[T ~int16](collection []T) T {
-	if archsimd.X86.AVX512() {
+	switch currentSimdFeature {
+	case simdFeatureAVX512:
 		return SumInt16x32(collection)
-	} else if archsimd.X86.AVX2() {
+	case simdFeatureAVX2:
 		return SumInt16x16(collection)
+	case simdFeatureAVX:
+		return SumInt16x8(collection)
+	default:
+		return lo.Sum(collection)
 	}
-	return lo.SumInt16x8(collection)
 }
 
 func SumInt32[T ~int32](collection []T) T {
-	if archsimd.X86.AVX512() {
+	switch currentSimdFeature {
+	case simdFeatureAVX512:
 		return SumInt32x16(collection)
-	} else if archsimd.X86.AVX2() {
+	case simdFeatureAVX2:
 		return SumInt32x8(collection)
+	case simdFeatureAVX:
+		return SumInt32x4(collection)
+	default:
+		return lo.Sum(collection)
 	}
-	return lo.SumInt32x4(collection)
 }
 
 func SumInt64[T ~int64](collection []T) T {
-	if archsimd.X86.AVX512() {
+	switch currentSimdFeature {
+	case simdFeatureAVX512:
 		return SumInt64x8(collection)
-	} else if archsimd.X86.AVX2() {
+	case simdFeatureAVX2:
 		return SumInt64x4(collection)
+	case simdFeatureAVX:
+		return SumInt64x2(collection)
+	default:
+		return lo.Sum(collection)
 	}
-	return lo.SumInt64x2(collection)
 }
 
 func SumUint8[T ~uint8](collection []T) T {
-	if archsimd.X86.AVX512() {
+	switch currentSimdFeature {
+	case simdFeatureAVX512:
 		return SumUint8x64(collection)
-	} else if archsimd.X86.AVX2() {
+	case simdFeatureAVX2:
 		return SumUint8x32(collection)
+	case simdFeatureAVX:
+		return SumUint8x16(collection)
+	default:
+		return lo.Sum(collection)
 	}
-	return lo.SumUint8x16(collection)
 }
 
 func SumUint16[T ~uint16](collection []T) T {
-	if archsimd.X86.AVX512() {
+	switch currentSimdFeature {
+	case simdFeatureAVX512:
 		return SumUint16x32(collection)
-	} else if archsimd.X86.AVX2() {
+	case simdFeatureAVX2:
 		return SumUint16x16(collection)
+	case simdFeatureAVX:
+		return SumUint16x8(collection)
+	default:
+		return lo.Sum(collection)
 	}
-	return lo.SumUint16x8(collection)
 }
 
 func SumUint32[T ~uint32](collection []T) T {
-	if archsimd.X86.AVX512() {
+	switch currentSimdFeature {
+	case simdFeatureAVX512:
 		return SumUint32x16(collection)
-	} else if archsimd.X86.AVX2() {
+	case simdFeatureAVX2:
 		return SumUint32x8(collection)
+	case simdFeatureAVX:
+		return SumUint32x4(collection)
+	default:
+		return lo.Sum(collection)
 	}
-	return lo.SumUint32x4(collection)
 }
 
 func SumUint64[T ~uint64](collection []T) T {
-	if archsimd.X86.AVX512() {
+	switch currentSimdFeature {
+	case simdFeatureAVX512:
 		return SumUint64x8(collection)
-	} else if archsimd.X86.AVX2() {
+	case simdFeatureAVX2:
 		return SumUint64x4(collection)
+	case simdFeatureAVX:
+		return SumUint64x2(collection)
+	default:
+		return lo.Sum(collection)
 	}
-	return lo.SumUint64x2(collection)
 }
 
 func SumFloat32[T ~float32](collection []T) T {
-	if archsimd.X86.AVX512() {
+	switch currentSimdFeature {
+	case simdFeatureAVX512:
 		return SumFloat32x16(collection)
-	} else if archsimd.X86.AVX2() {
+	case simdFeatureAVX2:
 		return SumFloat32x8(collection)
+	case simdFeatureAVX:
+		return SumFloat32x4(collection)
+	default:
+		return lo.Sum(collection)
 	}
-	return lo.SumFloat32x4(collection)
 }
 
 func SumFloat64[T ~float64](collection []T) T {
-	if archsimd.X86.AVX512() {
+	switch currentSimdFeature {
+	case simdFeatureAVX512:
 		return SumFloat64x8(collection)
-	} else if archsimd.X86.AVX2() {
+	case simdFeatureAVX2:
 		return SumFloat64x4(collection)
+	case simdFeatureAVX:
+		return SumFloat64x2(collection)
+	default:
+		return lo.Sum(collection)
 	}
-	return lo.SumFloat64x2(collection)
 }
