9c738be2a2a240889b613ae7b0f7f2164d6c5cb0
Samuel Berthe <dev@samuel-berthe.fr>
2026-03-07 18:49:51 +0100

    perf: use value receivers for If/Switch fluent chains
    Replace pointer receivers with value receivers on ifElse and switchCase
    types. This enables the compiler to fully inline the fluent chain calls,
    eliminating pointer indirection overhead.
    
    Benchstat (go1.24, darwin/arm64, -count=6 -cpu=1):
    
                    │   before    │        after         │
                    │   sec/op    │  sec/op   vs base    │
    If              0.5222n ± 7%  0.2545n ± 9%  -51.26% (p=0.002)
    IfElseIf        1.0710n ± 3%  0.2516n ± 3%  -76.50% (p=0.002)
    IfElseIfChain   1.9265n ± 1%  0.2524n ± 2%  -86.90% (p=0.002)
    Switch          1.5965n ± 2%  0.2525n ± 1%  -84.18% (p=0.002)
    SwitchChain     2.3940n ± 1%  0.2521n ± 1%  -89.47% (p=0.002)
    geomean          1.327n       0.2526n       -80.97%

---
+++ benchmark/condition_benchmark_test.go
@@ -0,0 +1,51 @@
+package benchmark
+
+import (
+	"testing"
+
+	"github.com/samber/lo"
+)
+
+func BenchmarkIf(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = lo.If(i%2 == 0, "even").Else("odd")
+	}
+}
+
+func BenchmarkIfElseIf(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = lo.If(i%3 == 0, "fizz").
+			ElseIf(i%3 == 1, "buzz").
+			Else("none")
+	}
+}
+
+func BenchmarkIfElseIfChain(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = lo.If(i%5 == 0, "a").
+			ElseIf(i%5 == 1, "b").
+			ElseIf(i%5 == 2, "c").
+			ElseIf(i%5 == 3, "d").
+			Else("e")
+	}
+}
+
+func BenchmarkSwitch(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = lo.Switch[int, string](i % 3).
+			Case(0, "zero").
+			Case(1, "one").
+			Default("other")
+	}
+}
+
+func BenchmarkSwitchChain(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = lo.Switch[int, string](i % 5).
+			Case(0, "zero").
+			Case(1, "one").
+			Case(2, "two").
+			Case(3, "three").
+			Default("other")
+	}
+}
